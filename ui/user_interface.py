36a53f665a1b12e4262b8c6acad21e71b47ac0bb
VoltedSwitch <wirelessvoltswitch@gmail.com>
2025-04-17 15:04:56 +0500

    Replace Input Function With Instant Input Function And Format

--- ui/user_interface.py
+++ ui/user_interface.py
@@ -2,7 +2,8 @@ import time
 from .user_interface_utility_functions import (
     clear_screen,
     hide_cursor,
-    show_cursor
+    show_cursor,
+    instant_input
 )
 from .colors import c
 from structure.tic_tac_toe import TicTacToe
@@ -15,24 +16,30 @@ class UserInterface:
     def __init__(self):
         self.tic_tac_toe: TicTacToe = TicTacToe()
 
-    def display_board(self, disable_positions_chart_view:bool=False) -> None:
-        if self.tic_tac_toe.is_computer_player_mode_on():
-            print(f"Computer Mode Difficulty: {self.tic_tac_toe.get_computer_mode_difficulty().capitalize()}")
-            print()
-        b: dict[str, str] = self.tic_tac_toe.board
-        print(f"{b['1']}|{b['2']}|{b['3']}")
-        print("-----")
-        print(f"{b['4']}|{b['5']}|{b['6']}")
-        print("-----")
-        print(f"{b['7']}|{b['8']}|{b['9']}")
-        if not disable_positions_chart_view:
-            print()
-            print("Positions:")
-            print("1|2|3")
-            print("-----")
-            print("4|5|6")
-            print("-----")
-            print("7|8|9")
+    def play_game(self) -> None:
+        while True:
+            if self.get_valid_versus_mode_selection() == "2":
+                player1_symbol_or_back = self.before_starting_computer_versus_player_mode()
+                if player1_symbol_or_back == self.GO_BACK:
+                    continue
+            else:
+                player1_symbol_or_back = self.before_starting_player_versus_player_mode()
+                if player1_symbol_or_back == self.GO_BACK:
+                    continue
+                
+            self.tic_tac_toe.set_player_symbols(player1_symbol_or_back)
+            while True:
+                position = self.get_valid_position_from_active_player()
+                self.tic_tac_toe.active_player_box_fill(position)
+
+                if self.tic_tac_toe.some_player_won():
+                    self.tic_tac_toe.set_winner(self.tic_tac_toe.get_active_player())
+                    if self.after_game_over() == self.START_OVER:
+                        break
+                elif self.tic_tac_toe.is_board_full():
+                    if self.after_game_over() == self.START_OVER:
+                        break
+                self.tic_tac_toe.set_turn_end_for_active_player()
 
     def after_game_over(self) -> None:
         print(self.tic_tac_toe.game_over_message())
@@ -42,7 +49,7 @@ class UserInterface:
         print()
         self.display_board()
         print()
-        again = input("Play again? (Y/N): ").strip().upper()
+        again = instant_input("Play again? (Y/N): ").strip().upper()
         clear_screen()
         if again == "Y":
             self.tic_tac_toe.reset_game()
@@ -88,7 +95,7 @@ class UserInterface:
                 show_cursor()
                 position = self.tic_tac_toe.get_valid_computer_position()
             else:
-                position = input(position_entry_statement).strip()
+                position = instant_input(position_entry_statement).strip()
             clear_screen()
 
             if self.tic_tac_toe.is_valid_box_position(position) and self.tic_tac_toe.is_box_empty(position):
@@ -106,7 +113,7 @@ class UserInterface:
             player = self.tic_tac_toe.get_active_player_call().capitalize()
             O = self.tic_tac_toe.VALID_ENTRY_OPTIONS[0]
             X = self.tic_tac_toe.VALID_ENTRY_OPTIONS[1]
-            option = input(f"{player} Select '{O}' or '{X}' to  continue or ({self.GO_BACK})ack: ").strip().upper()
+            option = instant_input(f"{player} Select '{O}' or '{X}' to  continue or ({self.GO_BACK})ack: ").strip().upper()
             clear_screen()
 
             if option == self.GO_BACK:
@@ -126,13 +133,13 @@ class UserInterface:
             self.display_board(disable_positions_chart_view=True)
             print()
 
-            mode = input(f"Select An Option:\n1. Easy\n2. Medium\n3. Hard\nor ({self.GO_BACK})ack\n\n> ").strip().upper()
+            mode_number = instant_input(f"Select An Option:\n1. Easy\n2. Medium\n3. Hard\nor ({self.GO_BACK})ack\n\n> ").strip().upper()
             clear_screen()
 
-            if mode == self.GO_BACK:
+            if mode_number == self.GO_BACK:
                 return self.GO_BACK
-            elif mode in ("1", "2", "3"):
-                return mode_difficulty[mode]
+            elif mode_number in ("1", "2", "3"):
+                return mode_difficulty[mode_number]
             print("Please Enter A Valid Mode Number OR B for Back!\n")
 
     def get_valid_versus_mode_selection(self) -> str:
@@ -140,34 +147,28 @@ class UserInterface:
             self.display_board(disable_positions_chart_view=True)
             print()
 
-            mode = input("Select An Option:\n1. Player vs Player\n2. Player vs Computer\n\n> ").strip()
+            mode_number = instant_input("Select An Option:\n1. Player vs Player\n2. Player vs Computer\n\n> ").strip()
             clear_screen()
 
-            if mode in ("1", "2"):
-                return mode
+            if mode_number in ("1", "2"):
+                return mode_number
             print("Please Enter A Valid Mode Number!\n")
-            
-    def play_game(self) -> None:
-        while True:
-            if self.get_valid_versus_mode_selection() == "2":
-                player1_symbol_or_back = self.before_starting_computer_versus_player_mode()
-                if player1_symbol_or_back == self.GO_BACK:
-                    continue
-            else:
-                player1_symbol_or_back = self.before_starting_player_versus_player_mode()
-                if player1_symbol_or_back == self.GO_BACK:
-                    continue
-                
-            self.tic_tac_toe.set_player_symbols(player1_symbol_or_back)
-            while True:
-                position = self.get_valid_position_from_active_player()
-                self.tic_tac_toe.active_player_box_fill(position)
 
-                if self.tic_tac_toe.some_player_won():
-                    self.tic_tac_toe.set_winner(self.tic_tac_toe.get_active_player())
-                    if self.after_game_over() == self.START_OVER:
-                        break
-                elif self.tic_tac_toe.is_board_full():
-                    if self.after_game_over() == self.START_OVER:
-                        break
-                self.tic_tac_toe.set_turn_end_for_active_player()
+    def display_board(self, disable_positions_chart_view:bool=False) -> None:
+        if self.tic_tac_toe.is_computer_player_mode_on():
+            print(f"Computer Mode Difficulty: {self.tic_tac_toe.get_computer_mode_difficulty().capitalize()}")
+            print()
+        b: dict[str, str] = self.tic_tac_toe.board
+        print(f"{b['1']}|{b['2']}|{b['3']}")
+        print("-----")
+        print(f"{b['4']}|{b['5']}|{b['6']}")
+        print("-----")
+        print(f"{b['7']}|{b['8']}|{b['9']}")
+        if not disable_positions_chart_view:
+            print()
+            print("Positions:")
+            print("1|2|3")
+            print("-----")
+            print("4|5|6")
+            print("-----")
+            print("7|8|9")
